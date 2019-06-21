2c50cd225cc00d22f085ad27ce0ec26b762de3a8
DKatsarski <dd.katsarski@gmail.com>
2019-06-21 16:47:21 +0300

    the frame of the crossword is done
    Starting from the same letter

--- Crossword/Crossword/Program.cs
+++ Crossword/Crossword/Program.cs
@@ -19,44 +19,68 @@ namespace Crossword
 
             var listOfRandomWords = new List<string>();
 
-            for (int i = 0; i < 10; i++)
-            {
-                int randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
 
-                var randomWord = englishWords[randomNumber];
+            int randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
 
-                listOfRandomWords.Add(randomWord);
-            }
+            var randomWord = englishWords[randomNumber];
 
 
             string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
             string[] vowels = { "a", "e", "i", "o", "u" };
             string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
-            string[,] testArray = new string[10, 22];
-            int[][] testJaggedArray = new int[3][];
+            string[,] testArray = new string[30, 30];
 
-            var numberBetween0And10 = new Random();
-
-            for (int row = 0; row < testArray.GetLength(0); row++)
+            for (int row = 0; row < 1; row++)
             {
                 for (int col = 0; col < testArray.GetLength(1); col++)
                 {
-                    int randomLetter = numberBetween0And10.Next(0, 26);
-                    testArray[row, col] = alphabet[randomLetter];
+                    
                 }
             }
 
+            for (int i = 0; i < testArray.GetLength(0); i++)
+            {
+                if (i < randomWord.Length)
+                {
+                    testArray[i, 0] = randomWord[i].ToString();
+
+                }
+
+            }
+
+            randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
+            randomWord = englishWords[randomNumber];
+
+
+            while (testArray[0, 0] != randomWord[0].ToString())
+            {
+                randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
+
+                randomWord = englishWords[randomNumber];
+            }
+
+            for (int i = 0; i < testArray.GetLength(1); i++)
+            {
+                if (i < randomWord.Length)
+                {
+                    testArray[0, i] = randomWord[i].ToString();
+
+                }
+
+            }
+
+
             for (int row = 0; row < testArray.GetLength(0); row++)
             {
                 for (int col = 0; col < testArray.GetLength(1); col++)
                 {
-                    Console.Write("{0} ", 
+                    Console.Write("{0} ",
                     testArray[row, col]);
                 }
                 Console.WriteLine();
             }
-
+           
         }
     }
 }
