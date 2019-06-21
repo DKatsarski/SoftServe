fc592e2849e57941bfb69b143908e5ba5a314cf3
DKatsarski <dd.katsarski@gmail.com>
2019-06-21 14:02:02 +0300

    alphabet added

--- Crossword/Crossword/Program.cs
+++ Crossword/Crossword/Program.cs
@@ -29,7 +29,11 @@ namespace Crossword
             }
 
 
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] vowels = { "a", "e", "i", "o", "u" };
+            string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
+            Console.WriteLine(alphabet.Length);
 
         }
     }
