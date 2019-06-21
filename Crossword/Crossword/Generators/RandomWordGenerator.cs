c8752a8e6e4f2a42e6934c89c7643653b98655ac
DKatsarski <dd.katsarski@gmail.com>
2019-06-21 18:20:51 +0300

    WordsOperator class introduced
    Random words and letters methods completed

---
+++ Crossword/Crossword/Generators/RandomWordGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Crossword.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossword.Generators
+{
+    public class WordsOperator
+    {
+        private const int MinIndexOfList = 0;
+        private IWords words;
+        private Random randomGenerator;
+        private IList<string> listOfWords;
+
+        public WordsOperator(IWords words)
+        {
+            this.words = words;
+            this.randomGenerator = new Random();
+            //important to be declared only just once to optimize resources
+            this.listOfWords = this.words.GetWords;
+        }
+
+        public string GenerateRandomWord()
+        {
+            int maxIndexOfWordsList = listOfWords.Count;
+            int randomIndexNumber = randomGenerator.Next(MinIndexOfList, maxIndexOfWordsList);
+            var randomWord = listOfWords[randomIndexNumber];
+            return randomWord;
+        }
+
+        public string GenerateRandomLetter(string[] alphabet)
+        {
+            int maxcIndexOfAlphabetList = alphabet.Length;
+            int randomIndexNuber = randomGenerator.Next(MinIndexOfList, maxcIndexOfAlphabetList);
+            var randomLetter = alphabet[randomIndexNuber];
+            return randomLetter;
+        }
+    }
+}
--- Crossword/Crossword/Program.cs
+++ Crossword/Crossword/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Crossword.Data;
+using Crossword.Generators;
 
 namespace Crossword
 {
@@ -13,73 +14,57 @@ namespace Crossword
         {
             IWords englishDictionary = new EnglishWordsDictionary();
 
-            var englishWords = englishDictionary.GetWords;
-
-            Random myRandomWords = new Random();
-
-            var listOfRandomWords = new List<string>();
+            var wordsOperator = new WordsOperator(englishDictionary);
 
 
-            int randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
 
-            var randomWord = englishWords[randomNumber];
-
-
-            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
-            string[] vowels = { "a", "e", "i", "o", "u" };
+            var randomWord = wordsOperator.GenerateRandomWord();
+         
             string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
-            string[,] testArray = new string[30, 30];
+            Console.WriteLine(wordsOperator.GenerateRandomLetter(alphabet));
 
-            for (int row = 0; row < 1; row++)
-            {
-                for (int col = 0; col < testArray.GetLength(1); col++)
-                {
-                    
-                }
-            }
-
-            for (int i = 0; i < testArray.GetLength(0); i++)
-            {
-                if (i < randomWord.Length)
-                {
-                    testArray[i, 0] = randomWord[i].ToString();
-
-                }
-
-            }
-
-            randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
-            randomWord = englishWords[randomNumber];
+            //string[,] testArray = new string[30, 30];
 
 
-            while (testArray[0, 0] != randomWord[0].ToString())
-            {
-                randomNumber = myRandomWords.Next(0, englishWords.Count - 1);
+            //for (int i = 0; i < testArray.GetLength(0); i++)
+            //{
+            //    if (i < randomWord.Length)
+            //    {
+            //        testArray[i, 0] = randomWord[i].ToString();
 
-                randomWord = englishWords[randomNumber];
-            }
+            //    }
 
-            for (int i = 0; i < testArray.GetLength(1); i++)
-            {
-                if (i < randomWord.Length)
-                {
-                    testArray[0, i] = randomWord[i].ToString();
+            //}
 
-                }
+            //randomWord = wordsOperator.GenerateRandomWord(); 
 
-            }
+            //while (testArray[0, 0] != randomWord[0].ToString())
+            //{
+            //    randomWord = wordsOperator.GenerateRandomWord();
+
+            //}
+
+            //for (int i = 0; i < testArray.GetLength(1); i++)
+            //{
+            //    if (i < randomWord.Length)
+            //    {
+            //        testArray[0, i] = randomWord[i].ToString();
+
+            //    }
+
+            //}
 
 
-            for (int row = 0; row < testArray.GetLength(0); row++)
-            {
-                for (int col = 0; col < testArray.GetLength(1); col++)
-                {
-                    Console.Write("{0} ",
-                    testArray[row, col]);
-                }
-                Console.WriteLine();
-            }
+            //for (int row = 0; row < testArray.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < testArray.GetLength(1); col++)
+            //    {
+            //        Console.Write("{0} ",
+            //        testArray[row, col]);
+            //    }
+            //    Console.WriteLine();
+            //}
            
         }
     }
